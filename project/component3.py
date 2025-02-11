ae3f4f7498b9fcdbe3289ae2fdb12df0963ae005
dtafe0 <daniel.ly4@studytafensw.edu.au>
2025-02-11 05:05:13 +0000

    intial commit

--- project/component3.py
+++ project/component3.py
@@ -3,37 +3,22 @@
 # This component displays a numbered list of the existing user
 #  accounts (not including their passwords).
 
-
-# combine username and password into one string
-login = username +","+ password
-
-# set flag to show login state
-valid_entry = False
+import os
+# trying to find directory of this script so can open accounts.txt
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_name = "accounts.txt"
+file_path = os.path.join(script_dir, file_name)
 
 # open the accounts.txt file
-file = open("accounts.txt", "r")
-
-# check if the file is opened in read only mode
-if file.mode == 'r':
-
+with open(file_path, 'r') as file:
     # read the entire file and return it as a list of strings
     # where each string represents a line from the file
     list = file.readlines()
 
     # iterate through each line using for loop
-    for eachLine in list:
-
-        # selection using equality operator
-        if eachLine.strip() == login:
-
-            # set flag for valid entry to true
-            valid_entry = True
-
-            # exit the loop
-            break
-
-    # show the result to the user
-    print("login for", username, "is", valid_entry)
+    for eachLine in enumerate(list, start=1):
+        #print(eachLine)
+        print(eachLine + "." + eachLine.split(",")[0])
 
 # close file
 file.close()
